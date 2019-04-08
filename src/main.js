aa1b908dfcdf64405b872a66da4ba912367c035e
GRANDSOFT\xingqx <xingqx@glodon.com>
2019-04-08 11:50:36 +0800

    0408

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import Plugins from './plugin'
 import Mock from './mock/index'
 import ElementUI from 'element-ui'
-import VueScroll from 'vuescroll'
-import 'vuescroll/dist/vuescroll.css'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/common.scss'
 import './assets/css/overwrite.scss'
@@ -15,7 +13,6 @@ Mock.mockData()
 Vue.config.productionTip = false
 Vue.use(Plugins)
 Vue.use(ElementUI)
-Vue.use(VueScroll, {ops: {bar: {background: '#C0C4CC'}}})
 
 /* eslint-disable no-new */
 new Vue({
